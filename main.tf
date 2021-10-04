8960804d7409c4ba0f69eb3ce77cfc23f27220be
EliMeitner <eli.meitner@toluna.com>
2021-10-04 07:11:13 +0300

    Update main.tf, main.tf, and variables.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,11 @@
-terraform {
-  required_providers {
-    mongodbatlas = {
-      source = "mongodb/mongodbatlas"
-      version = "0.9.0"
-    }
-  }
+
+module "mongo-cluster" {
+    source                = "./modules/mongocluster"
+    atlas_public_key      = var.atlas_public_key
+    atlas_private_key     = var.atlas_private_key
+    atlasprojectid        = var.atlasprojectid
+    app_name              = var.app_name
+    environment           = var.environment
+    atlas_num_of_replicas = var.atlas_num_of_replicas
+    atlas_region          = var.atlas_region
 }
-
-provider "mongodbatlas" {
-  public_key = var.atlas_public_key
-  private_key  = var.atlas_private_key
-}
-
-resource "mongodbatlas_cluster" "main" {
-  project_id                   = var.atlasprojectid
-  name                         = "${local.app_name}-${local.environment}"
-  num_shards                   = 1
-  replication_factor           = local.env_vars.atlas_num_of_replicas
-  provider_backup_enabled      = true
-  auto_scaling_disk_gb_enabled = true
-  mongo_db_major_version       = "4.2"
-
-  provider_name               = "AWS"
-  disk_size_gb                = 10
-  provider_disk_iops          = 1000
-  provider_volume_type        = "STANDARD"
-  provider_instance_size_name = "M10"
-  provider_region_name        = var.atlas_region
-  
-}
-
-resource "null_resource" "db_persist" {
-  triggers = {
-    address = "${mongodbatlas_cluster.main.srv_address}"
-  }
-  provisioner "local-exec" {
-    when    = destroy
-    on_failure = fail
-      command = <<-EOT
-        ${path.module}/mongo_actions.sh chorus mongo_backup ${self.triggers.address}
-      EOT
-  }
-  provisioner "local-exec" {
-      command = <<-EOT
-        ${path.module}/mongo_actions.sh chorus mongo_restore ${self.triggers.address}
-      EOT
-  }
-}
-
---
+++ modules/mongocluster/variables.tf
@@ -0,0 +1,33 @@
+variable "atlas_region" {
+  default     = "US_EAST_1"
+  description = "Atlas Region"
+}
+
+variable "atlasorgid" {
+  description = "Atlas Org ID"
+}
+
+variable "atlasprojectid" {
+  description = "Atlas Project ID"
+}
+
+variable "atlas_public_key" {
+  description = "Atlas Public API Key"
+}
+
+variable "atlas_private_key" {
+  description = "Atlas Private API Key"
+}
+
+variable "app_name" {
+  description = "Application name"
+}
+
+variable "environment" {
+  description = "Environment name"
+}
+
+variable "atlas_num_of_replicas" {
+  description = "Number of replicas"
+}
+
