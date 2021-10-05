41ba0f027e43b2a6c27a0882e935b81b06ac588a
EliMeitner <eli.meitner@toluna.com>
2021-10-05 12:46:24 +0300

    Update README.md and main.tf

--- README.md
+++ README.md
@@ -24,6 +24,12 @@ module "mongodb" {
   backup_on_destroy     = true
   restore_on_create     = true
   ip_whitelist          = local.ip_whitelist
+  atlas_num_of_shards         = 1
+  mongo_db_major_version      = "4.2"
+  disk_size_gb                = 10
+  provider_disk_iops          = 1000
+  provider_volume_type        = "STANDARD"
+  provider_instance_size_name = "M10"
 }
 ```
 
--- examples/mongodb-with-aws/main.tf
+++ examples/mongodb-with-aws/main.tf
@@ -23,4 +23,10 @@ module "mongodb" {
   backup_on_destroy     = true
   restore_on_create     = true
   ip_whitelist          = ["127.0.0.1","127.0.0.2","127.0.0.3"]
+  atlas_num_of_shards         = 1
+  mongo_db_major_version      = "4.2"
+  disk_size_gb                = 10
+  provider_disk_iops          = 1000
+  provider_volume_type        = "STANDARD"
+  provider_instance_size_name = "M10"
 }
