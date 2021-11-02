cfd3868fa2910a270b50fa6544be0e9d805234fe
EliMeitner <eli.meitner@toluna.com>
2021-11-02 16:31:26 +0200

    Update mongo_actions.sh

--- files/mongo_actions.sh
+++ files/mongo_actions.sh
@@ -237,7 +237,6 @@ mongo_restore() {
     aws s3 cp s3://${SERVICE_NAME}-${ENV_TYPE}-mongodb-dumps/$WORKSPACE/$DBNAME.tar /tmp/ 
     mkdir -p /tmp/dump
     tar xvf /tmp/$DBNAME.tar -C /tmp/dump
-    echo "Restoring ~/mongorestore --uri '$DBHOST' --gzip /tmp/dump"
     ~/mongorestore --uri $DBHOST --gzip /tmp/dump
     rm -rf /tmp/$DBNAME.tar /tmp/dump
   else
