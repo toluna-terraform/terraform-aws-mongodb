6d726520758f648e929ea387e42b9b77b9580476
EliMeitner <eli.meitner@toluna.com>
2022-01-12 12:42:48 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,3 +6,6 @@ output "s3_dump_file" {
     value = data.aws_s3_bucket_object.get_dump_data
 }
 
+output "mongo_endpoint_connection_sting" {
+    value = trimprefix(lookup(mongodbatlas_cluster.main.connection_strings[0].aws_private_link_srv,var.aws_vpce[each.key].outputs.vpce.id,mongodbatlas_cluster.main.srv_address),"mongodb+srv://")
+}
